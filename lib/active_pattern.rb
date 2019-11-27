755940e3b169883c953829d440ba7272b00d2e7f
kokuyouwind <kokuyouwind@gmail.com>
2019-11-27 18:33:01 +0900

    refact

--- lib/active_pattern.rb
+++ lib/active_pattern.rb
@@ -1,6 +1,3 @@
-require "active_pattern/version"
+require 'active_pattern/version'
 
-module ActivePattern
-  class Error < StandardError; end
-  # Your code goes here...
-end
+module ActivePattern; end
--- spec/active_pattern/version_spec.rb
+++ spec/active_pattern/version_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe ActivePattern do
-  it "has a version number" do
+  it 'has a version number' do
     expect(ActivePattern::VERSION).not_to be nil
   end
 end
